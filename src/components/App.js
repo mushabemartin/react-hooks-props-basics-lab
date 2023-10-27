da916b577819a3c557b0f206ebd67aacb47d2f95
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-10-27 12:19:00 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
 function App() {
+  const user = {
+    name: 'Your Name',
+    city: 'Your City',
+    bio: 'Your bio goes here',
+    github: 'https://github.com/yourgithub',
+    linkedin: 'https://www.linkedin.com/in/yourlinkedin',
+  };
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
